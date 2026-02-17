9eca97176848607e5a214992283f76935f8690d1
Tyampa <tyampa@example.com>
2026-02-17 16:35:15 +0300

    time

--- study_pkg/scripts/second_node.py
+++ study_pkg/scripts/second_node.py
@@ -8,8 +8,9 @@ def main(args=None):
     rclpy.init(args=args)                   # инициализация ROS 2
     node = Node('node2')               # создаём узел с именем hello_node
     t=time.ctime()
-    node.get_logger().info(t)
-    time.sleep(5)
+    while True:
+     node.get_logger().info(t)
+     time.sleep(5)
     rclpy.spin(node)                        # запускаем цикл обработки
     node.destroy_node()
     rclpy.shutdown()
