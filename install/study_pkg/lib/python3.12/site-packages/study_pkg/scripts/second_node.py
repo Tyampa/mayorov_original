a6f7f24d90a0f680845b760da15d8a7710c40a05
Tyampa <tyampa@example.com>
2026-02-24 14:42:15 +0300

    finish

--- install/study_pkg/lib/python3.12/site-packages/study_pkg/scripts/second_node.py
+++ install/study_pkg/lib/python3.12/site-packages/study_pkg/scripts/second_node.py
@@ -8,8 +8,11 @@ def main(args=None):
     rclpy.init(args=args)                   # инициализация ROS 2
     node = Node('node2')               # создаём узел с именем hello_node
     t=time.ctime()
-    node.get_logger().info(t)
-    time.sleep(5)
+    while True:
+        t=datetime.now()
+        time.strftime("%Y:%m:%d:%X", [t])
+        node.get_logger().info(time.strftime)
+        time.sleep(5)
     rclpy.spin(node)                        # запускаем цикл обработки
     node.destroy_node()
     rclpy.shutdown()
--- study_pkg/scripts/second_node.py
+++ study_pkg/scripts/second_node.py
@@ -3,14 +3,16 @@
 
 import rclpy
 from rclpy.node import Node
+from datetime import datetime
 import time
 def main(args=None):
     rclpy.init(args=args)                   # инициализация ROS 2
     node = Node('node2')               # создаём узел с именем hello_node
-    t=time.ctime()
     while True:
-     node.get_logger().info(t)
-     time.sleep(5)
+        t=datetime.now()
+        pin = t.strftime("%Y-%m-%d %H:%M:%S")
+        node.get_logger().info(pin)
+        time.sleep(5)
     rclpy.spin(node)                        # запускаем цикл обработки
     node.destroy_node()
     rclpy.shutdown()
